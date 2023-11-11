2fb32318eecaafbaf169ad479ee0bdcd31831b89
Thada Wangthammang <thada.wth@gmail.com>
2023-11-12 01:20:16 +0700

    Add postRun option to jobOptions and implement job3 with a step that uses output from job1

--- src/examples/multiple-jobs.ts
+++ src/examples/multiple-jobs.ts
@@ -1,6 +1,12 @@
 import { createWorkflow } from '../libs';
 
-let workflow = createWorkflow();
+let workflow = createWorkflow({
+  jobOptions: {
+    postRun: {
+      order: 'asc',
+    },
+  },
+});
 
 workflow
   .job({
@@ -15,4 +21,20 @@ workflow
     name: 'job2',
     needs: w => w.needs('job1'),
     job: w => w.job().inputs({ projectPath: '/name' }),
+  })
+  .job({
+    name: 'job3',
+    needs: w => w.needs('job1', 'job2'),
+    job: w =>
+      w
+        .job()
+        .inputs({ projectPath: '/name' })
+        .step({
+          name: 'step1',
+          run: ({ steps }) => {
+            // @ts-expect-error
+            const result = needs.job1.outputs.result;
+            return { a: 1 };
+          },
+        }),
   });
--- src/libs/workflow.ts
+++ src/libs/workflow.ts
@@ -1,22 +1,20 @@
-import { Job } from './job';
+import { Job, JobOption } from './job';
 
-export interface WorkflowOption {}
+export interface WorkflowOption {
+  jobOptions?: JobOption;
+}
 
 export class Workflow<WorkflowJobs extends Record<string, Job> = {}> {
   protected _jobs: WorkflowJobs = {} as WorkflowJobs;
 
   constructor(option?: WorkflowOption) {}
-  job<const TName extends string,  const TNeed extends keyof WorkflowJobs | undefined>(
-    params: {
-      name: TName;
-      job: (w: WorkflowHelper<WorkflowJobs>) => WorkflowJobs[TName];
-      needs?: (w: WorkflowHelper<WorkflowJobs>) => TNeed[];
-    },
-
-  ) {
+  job<const TName extends string, const TNeed extends keyof WorkflowJobs | undefined>(params: {
+    name: TName;
+    job: (w: WorkflowHelper<WorkflowJobs>) => WorkflowJobs[TName];
+    needs?: (w: WorkflowHelper<WorkflowJobs>) => TNeed[];
+  }) {
     return this as Workflow<WorkflowJobs & { [K in TName]: Job }>;
   }
-  
 }
 
 export function createWorkflow(option?: WorkflowOption) {
@@ -24,11 +22,12 @@ export function createWorkflow(option?: WorkflowOption) {
 }
 
 export class WorkflowHelper<WorkflowJobs extends Record<string, Job> = {}> {
+  constructor(protected option?: WorkflowOption) {}
   needs<const TNeed extends keyof WorkflowJobs>(...args: TNeed[]) {
     return args;
   }
 
-  job(){
-    return new Job();
+  job() {
+    return new Job(this.option?.jobOptions);
   }
 }
